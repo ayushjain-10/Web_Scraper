563f89ffc5f779d1719896cafdc4794758e6da49
Ayush Jain <ayushshreeshreemal@gmail.com>
2023-06-29 07:44:34 -0700

    first commit

---
+++ scrape.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"github.com/gocolly/colly"
+)
+
+type Article struct {
+	Title string
+}
+
+func main() {
+	c := colly.NewCollector()
+
+	var article Article
+
+	c.OnHTML("article.css-180b3ld h2", func(e *colly.HTMLElement) {
+		article.Title = e.Text
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
+
+	c.Visit("https://www.nytimes.com/section/sports")
+
+
+	fmt.Printf("First article title: %s\n", article.Title)
+
+	jsonData, err := json.Marshal(article)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("JSON: %s\n", jsonData)
+
+	err = ioutil.WriteFile("output.json", jsonData, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
